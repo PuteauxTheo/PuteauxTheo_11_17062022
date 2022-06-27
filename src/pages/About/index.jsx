b7de452ec822b4ff6286001bd88269479104221c
Theo Puteaux <theo.ptx@laposte.net>
2022-06-27 14:19:12 +0200

    ajout de style + mise en place du deroulement

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,27 +1,61 @@
 import bgAbout from "../../assets/bgAbout.jpg"
+import topArrow from "../../assets/topArrow.svg"
+import botArrow from "../../assets/botArrow.svg"
+import { useState } from "react"
+
 function About() {
+    const [isOpen, setIsOpen] = useState(false)
+    const toggle = () => setIsOpen(!isOpen)
     return(
         <div className="container-about">
-            <div className="bgAbout">
+            <div className="container-about-bgAbout">
                 <img src={bgAbout} alt=""/>
             </div>
-            <ul>
-                <li>
-                    <h2>Fiabilité</h2>
-                    <p> blablabla</p>
+            <ul className="container-about-list">
+                <li className="container-about-list-content">
+                    <div onClick={toggle} className="container-about-list-content-title">
+                        <h2>Fiabilité</h2>
+                        { isOpen ? <img src={topArrow} alt=""/> : <img src={botArrow} alt=""/>}
+                    </div>
+                    {isOpen && 
+                        <div className="container-about-list-content-text">                        
+                            <p>vfvfvfvfv</p>
+                        </div>       
+                    } 
                 </li>
-                <li>
-                    <h2>Respect</h2>
-                    <p>blablabla</p>
+                <li className="container-about-list-content">
+                    <div onClick={toggle} className="container-about-list-content-title">
+                        <h2>Respect</h2>
+                        { isOpen ? <img src={topArrow} alt=""/> : <img src={botArrow} alt=""/>}
+                    </div>
+                    {isOpen && 
+                        <div className="container-about-list-content-text">                        
+                            <p>vfvfvfvfv</p>
+                        </div>       
+                    } 
                 </li>
-                <li>
-                    <h2>Service</h2>
-                    <p>bsddfsdf</p>    
+                <li className="container-about-list-content">
+                    <div onClick={toggle} className="container-about-list-content-title">
+                        <h2>Service</h2>
+                        { isOpen ? <img src={topArrow} alt=""/> : <img src={botArrow} alt=""/>}
+                    </div>
+                    {isOpen && 
+                        <div className="container-about-list-content-text">                        
+                            <p>vfvfvfvfv</p>
+                        </div>       
+                    } 
                 </li>
-                <li>
-                    <h2>Responsabilité</h2>
-                    <p>dccdcdcd</p>
-                </li>
+                <li className="container-about-list-content">
+                    <div onClick={toggle} className="container-about-list-content-title">
+                        <h2>Responsabilité</h2>
+                        { isOpen ? <img src={topArrow} alt=""/> : <img src={botArrow} alt=""/>}
+                    </div>
+                    {isOpen && 
+                        <div className="container-about-list-content-text">                        
+                            <p>vfvfvfvfv</p>
+                        </div>       
+                    } 
+                </li>        
             </ul>
         </div>
     )
