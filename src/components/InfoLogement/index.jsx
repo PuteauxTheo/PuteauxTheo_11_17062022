465d05a36fafec42f6ad540fd8fdcd92a4dc9fe0
Theo Puteaux <theo.ptx@laposte.net>
2022-06-27 14:18:01 +0200

    ajout des class + mise en place du deroulement

--- src/components/InfoLogement/index.jsx
+++ src/components/InfoLogement/index.jsx
@@ -1,32 +1,62 @@
+import profile from "../../assets/profile.png"
+import topArrow from "../../assets/topArrow.svg"
+import botArrow from "../../assets/botArrow.svg"
+import starFull from "../../assets/starFull.svg"
+import starEmpty from "../../assets/starEmpty.svg"
+import { useState } from "react"
+
 function InfoLogement() {
+    const [isOpen, setIsOpen] = useState(false)
+    const toggle = () => setIsOpen(!isOpen)
     return (
         <div className="container-infoLogement">
-            <div className="infoLogement-presentation">
-                <h3>Title Cozy loft one the canal</h3>
-                <div className="infoLogement-name">
-                    <p>Alex Dumx</p>
-                    <p>photo du profile</p>
-                </div>
-            </div>
-            <p className="infoLogement-city">Paris, île de France</p>
-            <div className="infoLogement-filter">
-                <div className="infoLogement-filter-tag">
-                    <span>tag</span>
-                </div>
-                <p className="infoLogement-filter-star">****</p>
-            </div>
-            <ul className="infoLogement-list">
-                <li className="infoLogement-list-description">
-                    <div>
-                        <h4>Description</h4>        
+            <div className="container-infoLogement-presentation">
+                <h3 className="container-infoLogement-presentation-title">Title Cozy loft one the canal</h3>
+                <div className="container-infoLogement-presentation-profile">
+                    <div className="container-infoLogement-presentation-profile-name">
+                        <p>Alexandre Dumas</p>
                     </div>
-                    <p>vfvfvfvfv</p>
+                    <div className="container-infoLogement-presentation-profile-img">
+                        <img src={profile} alt="profile"/>
+                    </div>
+
+                </div>
+            </div>
+            <p className="container-infoLogement-city">Paris, île de France</p>
+            <div className="container-infoLogement-filter">
+                <div className="container-infoLogement-filter-tag">
+                    <span>nag</span>
+                </div>
+                <div>
+                    <img src={starFull} alt="etoile rempli"/>
+                    <img src={starFull} alt="etoile rempli"/>
+                    <img src={starFull} alt="etoile rempli"/>
+                    <img src={starFull} alt="etoile rempli"/>
+                    <img src={starEmpty} alt="etoile vide"/>
+
+                </div>
+                
+            </div>
+            <ul className="container-infoLogement-list">
+                <li className="container-infoLogement-list-content">
+                    <div onClick={toggle} className="container-infoLogement-list-content-title">
+                        <h4>Description</h4> 
+                        { isOpen ? <img src={topArrow} alt=""/> : <img src={botArrow} alt=""/>}    
+                    </div>
+                    {isOpen && 
+                        <div className="container-infoLogement-list-content-text">                        
+                            <p>vfvfvfvfv</p>
+                        </div>       
+                    }                               
                 </li>
-                <li className="infoLogement-list-equipements">
-                    <div>
+                <li className="container-infoLogement-list-content">
+                    <div className="container-infoLogement-list-content-title">
                         <h4>Equipements</h4>
+                        <img src={topArrow} alt="derouler vers le bas"/>
                     </div>
-                    <p>cdvfblgblgd</p>
+                    <div className="container-infoLogement-list-content-text">
+                        <p>vfvfvfvfv</p>
+                    </div> 
                 </li>                
             </ul>             
         </div>
