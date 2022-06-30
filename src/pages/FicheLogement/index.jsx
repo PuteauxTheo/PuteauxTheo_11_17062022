8f84203f6acae8a0c4d808a795c8ba45c07a5677
Theo Puteaux <theo.ptx@laposte.net>
2022-06-30 19:12:47 +0200

    ajout des données dynamique

--- src/pages/FicheLogement/index.jsx
+++ src/pages/FicheLogement/index.jsx
@@ -1,13 +1,21 @@
+import { useParams } from 'react-router-dom'
 import bgFicheLogement from '../../assets/bgFicheLogement.jpg'
 import InfoLogement from '../../components/InfoLogement'
+import logement from '../../data/logements.json'
 
 function FicheLogement() {
+
+    let id = useParams();
+
+    console.log(id)
+
+    const location = logement.find( location => location.id === id.locationID )
     return (
         <div className='container-ficheLogement'>
             <div className='bgFicheLogement'> 
                 <img src={bgFicheLogement} alt="salon"/>
             </div>
-            <InfoLogement/>
+            <InfoLogement location={ location }/>
             
         </div>
     )
