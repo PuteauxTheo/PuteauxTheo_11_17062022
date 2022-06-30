42ba57ee8129e49360ebea55c019a78fa4f8766d
Theo Puteaux <theo.ptx@laposte.net>
2022-06-30 12:34:55 +0200

    reorganisation du style pour les articles

--- src/components/Location/index.jsx
+++ src/components/Location/index.jsx
@@ -1,23 +1,18 @@
 import { Link } from "react-router-dom"
+import logement from "../../data/logements.json"
 
 function Location() {
     return (
         <div className="container-location">
-            <article className="container-location-article">
-                <Link to="location">
-                    <h3 className="container-location-article-title"> Titre de la Location</h3>
-                </Link>
-            </article>
-            <article className="container-location-article">
-                <Link to="/location">
-                    <h3 className="container-location-article-title"> Titre de la Location</h3>
-                </Link>            
-            </article>
-            <article className="container-location-article">
-                <Link to="/location">
-                    <h3 className="container-location-article-title"> Titre de la Location</h3>
-                </Link>
-            </article>
+            { logement.map( (logement) => (
+                <article className="container-location-article" key={logement.id}>
+                    <Link to={`location/${logement.id}`} className="container-location-link">
+                        <img src={logement.cover} className="container-location-link-cover" alt=""/>
+                        <div className="container-location-link-gradient"></div>
+                        <h3 className="container-location-link-title">{logement.title}</h3>
+                    </Link>
+                </article>
+            ))}        
         </div>
         
     )
