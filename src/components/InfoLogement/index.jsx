9c340b989b5e7e3db92460bbdceb6843196ff412
Theo Puteaux <theo.ptx@laposte.net>
2022-06-30 19:11:27 +0200

    mise en place des données dynamique pour les locations

--- src/components/InfoLogement/index.jsx
+++ src/components/InfoLogement/index.jsx
@@ -1,63 +1,52 @@
-import profile from "../../assets/profile.png"
-import topArrow from "../../assets/topArrow.svg"
-import botArrow from "../../assets/botArrow.svg"
+
 import starFull from "../../assets/starFull.svg"
 import starEmpty from "../../assets/starEmpty.svg"
-import { useState } from "react"
+import Dropdown from "../Dropdown"
 
-function InfoLogement() {
-    const [isOpen, setIsOpen] = useState(false)
-    const toggle = () => setIsOpen(!isOpen)
+function InfoLogement({ location }) {
+    let starTab =[ 1, 2, 3, 4, 5]
     return (
         <div className="container-infoLogement">
             <div className="container-infoLogement-presentation">
-                <h3 className="container-infoLogement-presentation-title">Title Cozy loft one the canal</h3>
+                <h3 className="container-infoLogement-presentation-title">{location.title}</h3>
                 <div className="container-infoLogement-presentation-profile">
                     <div className="container-infoLogement-presentation-profile-name">
-                        <p>Alexandre Dumas</p>
+                        <p>{location.host.name}</p>
                     </div>
                     <div className="container-infoLogement-presentation-profile-img">
-                        <img src={profile} alt="profile"/>
+                        <img src={location.host.picture} alt="profile"/>
                     </div>
 
                 </div>
             </div>
-            <p className="container-infoLogement-city">Paris, île de France</p>
+            <p className="container-infoLogement-city">{location.location}</p>
             <div className="container-infoLogement-filter">
                 <div className="container-infoLogement-filter-tag">
-                    <span>nag</span>
+                    {
+                        location.tags.map( (tag) => ( 
+                            <span key={tag}>{tag}</span> 
+                    ))}
                 </div>
                 <div>
-                    <img src={starFull} alt="etoile rempli"/>
-                    <img src={starFull} alt="etoile rempli"/>
-                    <img src={starFull} alt="etoile rempli"/>
-                    <img src={starFull} alt="etoile rempli"/>
-                    <img src={starEmpty} alt="etoile vide"/>
+                    {
+                        starTab.map(( el ) => (
+                            location.rating >= el ? <img key={el+"full"} src={starFull} alt="etoile rempli"/> : <img key={1+"empty"} src={starEmpty} alt="etoile vide"/>
+                        ))
+                    }
 
                 </div>
                 
             </div>
             <ul className="container-infoLogement-list">
-                <li className="container-infoLogement-list-content">
-                    <div onClick={toggle} className="container-infoLogement-list-content-title">
-                        <h4>Description</h4> 
-                        { isOpen ? <img src={topArrow} alt=""/> : <img src={botArrow} alt=""/>}    
-                    </div>
-                    {isOpen && 
-                        <div className="container-infoLogement-list-content-text">                        
-                            <p>vfvfvfvfv</p>
-                        </div>       
-                    }                               
-                </li>
-                <li className="container-infoLogement-list-content">
-                    <div className="container-infoLogement-list-content-title">
-                        <h4>Equipements</h4>
-                        <img src={topArrow} alt="derouler vers le bas"/>
-                    </div>
-                    <div className="container-infoLogement-list-content-text">
-                        <p>vfvfvfvfv</p>
-                    </div> 
-                </li>                
+                <Dropdown dropDownTitle="Description">
+                    <p className="description-text">{location.description}</p>
+                </Dropdown>
+                <Dropdown dropDownTitle="Equipements">
+                    {
+                        location.equipments.map((equipment) => (
+                            <p key={equipment} className="equipement-text">{equipment}</p>
+                    ))}
+                </Dropdown>                
             </ul>             
         </div>
     )
