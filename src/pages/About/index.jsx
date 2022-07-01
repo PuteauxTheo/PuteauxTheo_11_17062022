ce96faeb5c42ccda158e66a057835d787f76c3b9
Theo Puteaux <theo.ptx@laposte.net>
2022-07-01 14:07:14 +0200

    dropDown de la page about mis en place

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,61 +1,30 @@
 import bgAbout from "../../assets/bgAbout.jpg"
-import topArrow from "../../assets/topArrow.svg"
-import botArrow from "../../assets/botArrow.svg"
-import { useState } from "react"
+import Dropdown from "../../components/Dropdown"
 
 function About() {
-    const [isOpen, setIsOpen] = useState(false)
-    const toggle = () => setIsOpen(!isOpen)
+    const aboutFiability = "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.";
+    const aboutRespect = "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.";
+    const aboutService = "Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question.";
+    const aboutSecurity = "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.";
+
     return(
         <div className="container-about">
             <div className="container-about-bgAbout">
                 <img src={bgAbout} alt=""/>
             </div>
             <ul className="container-about-list">
-                <li className="container-about-list-content">
-                    <div onClick={toggle} className="container-about-list-content-title">
-                        <h2>Fiabilité</h2>
-                        { isOpen ? <img src={topArrow} alt=""/> : <img src={botArrow} alt=""/>}
-                    </div>
-                    {isOpen && 
-                        <div className="container-about-list-content-text">                        
-                            <p>vfvfvfvfv</p>
-                        </div>       
-                    } 
-                </li>
-                <li className="container-about-list-content">
-                    <div onClick={toggle} className="container-about-list-content-title">
-                        <h2>Respect</h2>
-                        { isOpen ? <img src={topArrow} alt=""/> : <img src={botArrow} alt=""/>}
-                    </div>
-                    {isOpen && 
-                        <div className="container-about-list-content-text">                        
-                            <p>vfvfvfvfv</p>
-                        </div>       
-                    } 
-                </li>
-                <li className="container-about-list-content">
-                    <div onClick={toggle} className="container-about-list-content-title">
-                        <h2>Service</h2>
-                        { isOpen ? <img src={topArrow} alt=""/> : <img src={botArrow} alt=""/>}
-                    </div>
-                    {isOpen && 
-                        <div className="container-about-list-content-text">                        
-                            <p>vfvfvfvfv</p>
-                        </div>       
-                    } 
-                </li>
-                <li className="container-about-list-content">
-                    <div onClick={toggle} className="container-about-list-content-title">
-                        <h2>Responsabilité</h2>
-                        { isOpen ? <img src={topArrow} alt=""/> : <img src={botArrow} alt=""/>}
-                    </div>
-                    {isOpen && 
-                        <div className="container-about-list-content-text">                        
-                            <p>vfvfvfvfv</p>
-                        </div>       
-                    } 
-                </li>        
+                <Dropdown dropDownTitle="Fiabilité">
+                    <p>{aboutFiability}</p>
+                </Dropdown>
+                <Dropdown dropDownTitle="Respect">
+                    <p>{aboutRespect}</p>
+                </Dropdown>
+                <Dropdown dropDownTitle="Service">
+                    <p>{aboutService}</p>
+                </Dropdown>
+                <Dropdown dropDownTitle="Sécurité">
+                    <p>{aboutSecurity}</p>
+                </Dropdown>
             </ul>
         </div>
     )
