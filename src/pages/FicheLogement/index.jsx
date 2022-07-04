e4250750dac0add96f4c31c181f16be068ea8c54
Theo Puteaux <theo.ptx@laposte.net>
2022-07-04 23:47:54 +0200

    ajout du background sombre pour la banniere

--- src/pages/FicheLogement/index.jsx
+++ src/pages/FicheLogement/index.jsx
@@ -1,13 +1,13 @@
 import { useParams } from 'react-router-dom'
 import Caroussel from '../../components/Caroussel';
 import InfoLogement from '../../components/InfoLogement'
-import logement from '../../data/logements.json'
+import logements from '../../data/logements.json'
 
 function FicheLogement() {
 
     let id = useParams();
 
-    const location = logement.find( location => location.id === id.locationID )
+    const location = logements.find( location => location.id === id.locationID )
     return (
         <div className='container-ficheLogement'>  
             <div className='caroussel'>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -6,6 +6,7 @@ function Home() {
         <div className="container-home">
             <div className="container-home-bannerHome">
                 <img src={bgHome} alt="mer avec des rochers "/>
+                <div className="container-home-bannerHome-bg"></div>
                 <p className="container-home-bannerHome-text">Chez vous, partout et ailleurs</p>
             </div>
             <Location/>
