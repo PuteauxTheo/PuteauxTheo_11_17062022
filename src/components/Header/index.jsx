3bfeafcd5de7acfae11e7deeb07eb9b413c6cc76
Theo Puteaux <theo.ptx@laposte.net>
2022-07-04 23:46:44 +0200

    ajustement pour correspondre a la maquette

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,9 +4,9 @@ import logoHeader from '../../assets/logoHeader.png'
 
 function Header() {
     return( 
-        <header> 
-            <img src={logoHeader} alt='logo du site Web Kasa haut de page'/>
+        <header>             
             <nav> 
+                <Link to="/"><img className="menuImg"src={logoHeader} alt='logo du site Web Kasa haut de page'/></Link>
                 <ul className='menuHeader'>
                     <li><Link to="/"> Accueil</Link></li>
                     <li><Link to="/about"> A propos</Link></li>
--- src/components/InfoLogement/index.jsx
+++ src/components/InfoLogement/index.jsx
@@ -6,39 +6,39 @@ import Dropdown from "../Dropdown"
 function InfoLogement({ location }) {
     let starTab =[ 1, 2, 3, 4, 5]
     return (
-        <div className="container-infoLogement">
-            <div className="container-infoLogement-presentation">
-                <h3 className="container-infoLogement-presentation-title">{location.title}</h3>
-                <div className="container-infoLogement-presentation-profile">
-                    <div className="container-infoLogement-presentation-profile-name">
-                        <p>{location.host.name}</p>
+        <div className="content-infoLogement">
+            <div className="container-infoLogement">
+                <div className="container-infoLogement-presentation">
+                    <h3 className="container-infoLogement-presentation-title">{location.title}</h3>
+                    <p className="container-infoLogement-presentation-city">{location.location}</p>
+                    <div className="container-infoLogement-presentation-tag">
+                        {
+                            location.tags.map( (tag) => ( 
+                                <span key={tag}>
+                                    <p>{tag}</p>
+                                </span> 
+                        ))}
+                    </div>                
+                </div>
+                <div className="container-infoLogement-host">
+                    <div className="container-infoLogement-host-profile">
+                        <div className="container-infoLogement-host-profile-name">
+                            <p>{location.host.name}</p>
+                        </div>
+                        <div className="container-infoLogement-host-profile-img">
+                            <img src={location.host.picture} alt="profile"/>
+                        </div>
                     </div>
-                    <div className="container-infoLogement-presentation-profile-img">
-                        <img src={location.host.picture} alt="profile"/>
-                    </div>
+                    <div className="container-infoLogement-host-star">
+                        {
+                            starTab.map(( el ) => (
+                                location.rating >= el ? <img className="container-infoLogement-host-star-img" key={el+"full"} src={starFull} alt="etoile rempli"/> : <img className="container-infoLogement-host-star-img" key={el+"empty"} src={starEmpty} alt="etoile vide"/>
+                            ))
+                        }
 
+                    </div>                    
                 </div>
-            </div>
-            <p className="container-infoLogement-city">{location.location}</p>
-            <div className="container-infoLogement-filter">
-                <div className="container-infoLogement-filter-tag">
-                    {
-                        location.tags.map( (tag) => ( 
-                            <span key={tag}>
-                                <p>{tag}</p>
-                            </span> 
-                    ))}
-                </div>
-                <div>
-                    {
-                        starTab.map(( el ) => (
-                            location.rating >= el ? <img className="container-infoLogement-filter-star"key={el+"full"} src={starFull} alt="etoile rempli"/> : <img key={el+"empty"} src={starEmpty} alt="etoile vide"/>
-                        ))
-                    }
-
-                </div>
-                
-            </div>
+            </div>            
             <ul className="container-dropDown-list">
                 <Dropdown dropDownTitle="Description">
                     <p className="description-text">{location.description}</p>
