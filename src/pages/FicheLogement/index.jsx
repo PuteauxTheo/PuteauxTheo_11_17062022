4813bf74501803d85d10199e9d14d68e5f147e9b
PuteauxTheo <91119961+PuteauxTheo@users.noreply.github.com>
2022-07-06 10:38:13 +0200

    changement de nom de variable

--- src/pages/FicheLogement/index.jsx
+++ src/pages/FicheLogement/index.jsx
@@ -1,25 +1,42 @@
 import { useParams } from 'react-router-dom'
+// import { useState, useEffect } from 'react'
 import Caroussel from '../../components/Caroussel';
 import InfoLogement from '../../components/InfoLogement'
 import logements from '../../data/logements.json'
 
 function FicheLogement() {
 
-    let id = useParams();
+    // const [dataLogements, setDataLogements] = useState([]);
+    // console.log(dataLogements);     
+    // const getDataLogements = () =>{
+    //     fetch('logement.json' ,{headers: 
+    //         {'Content-Type': 'application/json','Accept': 'application/json'}
+    //     })
+    //         .then((response) => response.json())
+    //         .then((json) => setDataLogements(json))
+    //         .catch((error) => console.error(error))
+    // } 
 
-    const location = logements.find( location => location.id === id.locationID )
+    // useEffect(() => {
+    //     getDataLogements()
+    // }, []);
+
+    let id = useParams();
+    // console.log(dataLogements.length)
+    // const logement = dataLogements.find( logement => logement.id === id.logementID )
+    const logement = logements.find( logement => logement.id === id.logementID )
     return (
         <div className='container-ficheLogement'>  
             <div className='caroussel'>
                 <Caroussel>
                     {
-                        location.pictures.map( (picture) => (
+                        logement.pictures.map( (picture) => (
                             <img key={picture} src={picture} alt="caroussel" />
                     ))}
                 </Caroussel> 
             </div>              
                        
-            <InfoLogement location={ location }/>
+            <InfoLogement logement={ logement }/>
             
         </div>
     )
