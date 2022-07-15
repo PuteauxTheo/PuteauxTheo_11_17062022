7f6a7659416fa767abb480b5215a77ff6b0e1c5d
Theo Puteaux <theo.ptx@laposte.net>
2022-07-15 11:24:53 +0200

    test service

--- src/service/serviceMock.js
+++ src/service/serviceMock.js
@@ -1,31 +1,22 @@
 import logements  from '../data/logements.json'
 
 function getLogement(id){
-    const data = logements.find( logement => logement.id === id.logementID )
-    // const data = logements.find( logement => logement.id === "c67ab8a7")
-    return data;
-
+    const data = logements.find( logement => logement.id.toString() === id.logementID )
+    const test = {data};
+    return test;
 }
 
 // function getAllLogement(){
-//     const allLogement = { logements }
+//     const allLogement = logements
 //     return allLogement
 // }
 
-// const getData = async (id) =>{
-//     // switch (id) {
-//     //     case (id == null): return getAllLogement();
-//     //     case (id != null): return getLogement(id);
-//     //     default: return (id==null);
-//     // }   
-//     return getLogement(id);
-// }
 
-async function getData(id){
+const getData = async (id) => {
     // switch (id) {
     //     case (id == null): return getAllLogement();
     //     case (id != null): return getLogement(id);
-    //     default: return (id==null);
+    //     default: return getAllLogement();
     // }   
     return getLogement(id);
 }
